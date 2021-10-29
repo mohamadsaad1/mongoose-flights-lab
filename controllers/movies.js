8c375b818abf9846832297091fefe2cb7dffe934
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-10-29 11:05:42 -0500

    implement validation

--- controllers/movies.js
+++ controllers/movies.js
@@ -15,27 +15,42 @@ function create(req, res) {
   if (req.body.cast) {
     req.body.cast = req.body.cast.split(", ")
   }
+  console.log(req.body)
+  console.log(req.body.key)
+  for (let key in req.body) {
+    // on the first iteration
+    // req.body[key] === req.body.title
+    // on the second iteration
+    /// req.body[key] === req.body.releaseYear
+    if (req.body[key] === "") {
+      delete req.body[key]
+    }
+  }
+  console.log(req.body)
 
   //! Two methods of creating movies below!
   //* Creating the movie using the Movie Model
   Movie.create(req.body, function(error, movie){
-    console.log("The error, if one occured:", error)
-    console.log("The moview you just added:", movie)
+    if (error) {
+      console.log(error)
+      return res.redirect("/movies/new")
+    }
     res.redirect("/movies")
   })
 
   //* Creating the movie by instantiating a document using the Movie Model
   // const movie = new Movie(req.body)
   // movie.save(function(err) {
-  //   if (err) return res.redirect("/movies/new")
+  //   if (err){
+  //     console.log(err)
+  //     return res.redirect("/movies/new")
+  //   } 
   //   res.redirect("/movies/new")
   // })
 }
 
 function index(req, res) {
   Movie.find({}, function(error, movies) {
-    console.log("An error if there is one:", error)
-    console.log("The movies:", movies)
     res.render("movies/index", {
       movies,
       error,
--- models/movie.js
+++ models/movie.js
@@ -3,9 +3,21 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const movieSchema = new Schema({
-  title: String,
-  releaseYear: Number,
-  mpaaRating: String,
+  title: {
+    type: String,
+    required: true
+  },
+  releaseYear: {
+    type: Number, 
+    default: function() {
+      return new Date().getFullYear()
+    },
+    min: 1927,
+  },
+  mpaaRating: {
+    type: String,
+    enum: ["G", "PG", "PG-13", "R"]
+  },
   cast: [String],
   nowShowing: Boolean,
 }, {
